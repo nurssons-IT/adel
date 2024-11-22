4c024cd67e281f6774f74afb5bd18f7dfbd3baad
nurssons-it <nurhusein11@gmail.com>
2024-11-22 15:48:58 +0100

    aa

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -21,12 +21,13 @@ export const About = () => {
         </Helmet>
         <Row className="mb-5 mt-3 pt-md-3">
           <Col lg="8">
-            <h1 className="display-4 mb-4">Om mig</h1>
+            <h1 className="display-4 mb-4">Om mig!</h1>
             <hr className="t_border my-4 ml-0 text-left" />
           </Col>
         </Row>
         <Row className="sec_sp">
           <Col lg="5">
+          Hej!
             <h3 className="color_sec py-4">{dataabout.title}</h3>
           </Col>
           <Col lg="7" className="d-flex align-items-center">
